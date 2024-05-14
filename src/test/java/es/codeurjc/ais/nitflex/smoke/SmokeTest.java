81f06ecbd79de39b5539228954f941f8c9f6c91a
PoweeR02 <davidgc1911@gmail.com>
2024-05-14 12:58:20 +0200

    Workflow 3

--- src/test/java/es/codeurjc/ais/nitflex/smoke/SmokeTest.java
+++ src/test/java/es/codeurjc/ais/nitflex/smoke/SmokeTest.java
@@ -1,57 +1,29 @@
 package es.codeurjc.ais.nitflex.smoke;
 
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.HttpClients;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 class SmokeTest {
-
     @Test
-    void testWelcomeMessage() {
+    public void verifyApplicationDeployment() {
         String host = System.getProperty("host");
         if (host == null || host.isEmpty()) {
-            fail("La propiedad del sistema 'host' no está configurada. Asegúrate de pasar la URL de la aplicación correctamente.");
+            fail("El parametro 'host' no ha sido especificado. Por favor, proporcione el host donde esta desplegada la aplicacion");
         }
 
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        }
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
 
-        String welcomeMessage = getWelcomeMessageFromApp(host);
-        assertEquals("Welcome to my application", welcomeMessage, "El mensaje de bienvenida no coincide");
-    }
-
-    private String getWelcomeMessageFromApp(String host) {
-        HttpClient httpClient = HttpClients.createDefault();
-        HttpGet request = new HttpGet(host);
-
-        try {
-            HttpResponse response = httpClient.execute(request);
-            int statusCode = response.getStatusLine().getStatusCode();
-            if (statusCode != 200) {
-                fail("La solicitud a la aplicación falló con el código de estado: " + statusCode);
-            }
-
-            BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
-            StringBuilder result = new StringBuilder();
-            String line;
-            while ((line = reader.readLine()) != null) {
-                result.append(line);
-            }
-            return result.toString();
-        } catch (IOException e) {
-            fail("Error al realizar la solicitud a la aplicación: " + e.getMessage());
-            return null;
-        }
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("http://ais-anuncios.westeurope.azurecontainer.io:8080/");
+        String welcomeMessage = driver.findElement(By.id("create-film")).getText();
+        driver.quit();
+        assertEquals("New film", welcomeMessage);
     }
 }
