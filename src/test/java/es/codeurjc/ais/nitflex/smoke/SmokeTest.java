bdb2d9ffed9a140252ea7492dbbaeab42bd76ff0
PoweeR02 <davidgc1911@gmail.com>
2024-05-14 13:09:57 +0200

    Workflow 3 completado

--- src/test/java/es/codeurjc/ais/nitflex/smoke/SmokeTest.java
+++ src/test/java/es/codeurjc/ais/nitflex/smoke/SmokeTest.java
@@ -10,20 +10,31 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 class SmokeTest {
+
     @Test
     public void verifyApplicationDeployment() {
         String host = System.getProperty("host");
-        if (host == null || host.isEmpty()) {
-            fail("El parametro 'host' no ha sido especificado. Por favor, proporcione el host donde esta desplegada la aplicacion");
-        }
+        checkHostParameter(host);
 
+        WebDriver driver = setupWebDriver();
+        try {
+            driver.get(host);
+            String welcomeMessage = driver.findElement(By.id("create-film")).getText();
+            assertEquals("New film", welcomeMessage);
+        } finally {
+            driver.quit();
+        }
+    }
+
+    private void checkHostParameter(String host) {
+        if (host == null || host.isEmpty()) {
+            fail("Se requiere el parámetro 'host' para ejecutar la prueba. Por favor, asegúrese de proporcionar el host donde está desplegada la aplicación.");
+        }
+    }
+
+    private WebDriver setupWebDriver() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");
-
-        WebDriver driver = new ChromeDriver(options);
-        driver.get("http://ais-anuncios.westeurope.azurecontainer.io:8080/");
-        String welcomeMessage = driver.findElement(By.id("create-film")).getText();
-        driver.quit();
-        assertEquals("New film", welcomeMessage);
+        return new ChromeDriver(options);
     }
 }
