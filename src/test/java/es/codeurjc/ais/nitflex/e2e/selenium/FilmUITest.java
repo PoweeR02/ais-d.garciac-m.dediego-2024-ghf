5b373746a17f5a848232bf2dc969181e64f35e67
PoweeR02 <davidgc1911@gmail.com>
2024-05-14 20:25:12 +0200

    Workflow 4

--- src/test/java/es/codeurjc/ais/nitflex/e2e/selenium/FilmUITest.java
+++ src/test/java/es/codeurjc/ais/nitflex/e2e/selenium/FilmUITest.java
@@ -7,9 +7,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -17,7 +14,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -41,7 +37,6 @@ public class FilmUITest {
     protected static Map<String, List<String>> OperativeSystemToBrowser;
     protected WebDriver driver;
     protected WebDriverWait wait;
-    private String baseUrl;
 
     private WebDriver getDriver() {
         WebDriver driver = null;
@@ -120,84 +115,6 @@ public class FilmUITest {
         // THEN: Esperamos que la película creada aparezca en la nueva página resultante
 
         this.wait.until(ExpectedConditions.textToBe(By.id("film-title"), title));
-    }
-
-    @Test
-    void bugDetectionTest() throws InterruptedException {
-        // Given
-        String filmName = "In time";
-        String filmSynopsis = "El hallazgo de una formula contra el envejecimiento trae consigo no solo superpoblacion, sino tambien la transformacion del tiempo en moneda de cambio que permite sufragar tanto lujos como necesidades. En este contexto, Will, un obrero que consigue casualmente una inmensa cantidad de tiempo, debe huir de unos policias corruptos, con una rehen adinerada.";
-        driver.get(baseUrl);
-        driver.findElement(By.id("create-film")).click();
-        WebElement titleInput = driver.findElement(By.name("title"));
-        titleInput.sendKeys(filmName);
-        WebElement yearInput = driver.findElement(By.name("releaseYear"));
-        yearInput.sendKeys("2011");
-        WebElement urlInput = driver.findElement(By.name("url"));
-        urlInput.sendKeys("https://pics.filmaffinity.com/In_Time-139117348-large.jpg");
-        WebElement synopsisInput = driver.findElement(By.name("synopsis"));
-        synopsisInput.sendKeys(filmSynopsis);
-        driver.findElement(By.id("Save")).submit();
-
-        // When
-        driver.findElement(By.id("edit-film")).click();
-        driver.findElement(By.id("Cancel")).click();
-
-        // Then
-        assertThat(driver.findElement(By.id("film-title")).getText()).isEqualTo(filmName);
-        assertThat(driver.findElement(By.id("film-synopsis")).getText()).isEqualTo(filmSynopsis);
-    }
-
-    @Test
-    void newMovieCreationTest() throws InterruptedException {
-        // Given
-        String filmName = "Oppenheimer";
-        String filmSynopsis = "Durante la Segunda Guerra Mundial, el teniente general Leslie Groves designo al fisico J. Robert Oppenheimer para un grupo de trabajo que esta desarrollando el Proyecto Manhattan, cuyo objetivo consiste en fabricar la primera bomba atomica.";
-        driver.get(baseUrl);
-
-        // When
-        driver.findElement(By.id("create-film")).click();
-        WebElement titleInput = driver.findElement(By.name("title"));
-        titleInput.sendKeys(filmName);
-        WebElement yearInput = driver.findElement(By.name("releaseYear"));
-        yearInput.sendKeys("2023");
-        WebElement urlInput = driver.findElement(By.name("url"));
-        urlInput.sendKeys("https://pics.filmaffinity.com/Oppenheimer-828933592-large.jpg");
-        WebElement synopsisInput = driver.findElement(By.name("synopsis"));
-        synopsisInput.sendKeys(filmSynopsis);
-        driver.findElement(By.id("Save")).submit();
-
-        // Then
-        assertThat(driver.findElement(By.id("film-title")).getText()).isEqualTo(filmName);
-        assertThat(driver.findElement(By.id("film-synopsis")).getText()).isEqualTo(filmSynopsis);
-    }
-
-    @Test
-    void movieDeletionConfirmationTest() throws InterruptedException {
-        // Given
-        String filmName = "La sociedad de la nieve";
-        driver.get(baseUrl);
-        driver.findElement(By.id("create-film")).click();
-        WebElement titleInput = driver.findElement(By.name("title"));
-        titleInput.sendKeys(filmName);
-        WebElement yearInput = driver.findElement(By.name("releaseYear"));
-        yearInput.sendKeys("2023");
-        WebElement urlInput = driver.findElement(By.name("url"));
-        urlInput.sendKeys("https://pics.filmaffinity.com/La_sociedad_de_la_nieve-826551508-large.jpg");
-        WebElement synopsisInput = driver.findElement(By.name("synopsis"));
-        synopsisInput.sendKeys(
-                "Un avion uruguayo en el que viajan los jugadores del equipo de rugby del Old Christians Club de Montevideo se estrella en la cordillera de los Andes y los supervivientes deben sobreponerse a las condiciones extremas para mantenerse vivos.");
-        driver.findElement(By.id("Save")).submit();
-
-        // When
-        driver.findElement(By.id("remove-film")).click();
-        String messageCorrectRemove = driver.findElement(By.id("positiveMessage")).getText();
-        driver.findElement(By.id("all-films")).click();
-
-        // Then
-        assertThat(messageCorrectRemove).isEqualTo("Film '" + filmName + "' deleted");
-        // Asumimos que NO puede haber dos peliculas con el mismo titulo
-        assertTrue(driver.findElements(By.linkText(filmName)).isEmpty());
     }
 
 }
