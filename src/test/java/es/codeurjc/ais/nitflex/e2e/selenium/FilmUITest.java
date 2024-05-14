29f07d647280a1cddcac0322275707fea9fed595
PoweeR02 <davidgc1911@gmail.com>
2024-05-14 19:44:21 +0200

    Workflow 3 y 4

--- src/test/java/es/codeurjc/ais/nitflex/e2e/selenium/FilmUITest.java
+++ src/test/java/es/codeurjc/ais/nitflex/e2e/selenium/FilmUITest.java
@@ -1,11 +1,9 @@
 package es.codeurjc.ais.nitflex.e2e.selenium;
 
 import java.time.Duration;
+import java.util.*;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -28,36 +26,50 @@ public class FilmUITest {
     @LocalServerPort
     int port;
 
-    protected String browser;
     protected WebDriver driver;
     protected WebDriverWait wait;
+    public static Map<String, List<String>> OperativeSystemToBrowser;
+
+    private WebDriver getDriver() {
+        WebDriver driver = null;
+        String browser = System.getProperty("browser");
+        String os = System.getProperty("os");
+        if (OperativeSystemToBrowser.get(os).contains(browser)) {
+            switch (browser) {
+                case "safari":
+                    driver = new SafariDriver();
+                    break;
+                case "chrome":
+                    ChromeOptions chromeOptions = new ChromeOptions();
+                    chromeOptions.addArguments("--headless");
+                    driver = new ChromeDriver(chromeOptions);
+                    break;
+                case "edge":
+                    EdgeOptions options = new EdgeOptions();
+                    options.addArguments("--headless");
+                    driver = new EdgeDriver(options);
+                    break;
+                default:
+                    FirefoxOptions firefoxOptions = new FirefoxOptions();
+                    firefoxOptions.addArguments("--headless");
+                    driver = new FirefoxDriver(firefoxOptions);
+            }
+        }
+        return driver;
+    }
+
+    @BeforeAll
+    public static void initialiceMap() {
+        OperativeSystemToBrowser = new HashMap<>();
+        OperativeSystemToBrowser.put("macos-latest", new ArrayList<>(Arrays.asList("safari", "chrome", "firefox")));
+        OperativeSystemToBrowser.put("ubuntu-latest", new ArrayList<>(Arrays.asList("chrome", "firefox")));
+        OperativeSystemToBrowser.put("windows-latest", new ArrayList<>(Arrays.asList("edge", "chrome", "firefox")));
+    }
 
     @BeforeEach
     public void setup() {
-        browser = System.getProperty("browser", "chrome");
-        switch (browser) {
-            case "chrome":
-                ChromeOptions chromeOptions = new ChromeOptions();
-                chromeOptions.addArguments("--headless");
-                driver = new ChromeDriver(chromeOptions);
-                break;
-            case "firefox":
-                FirefoxOptions firefoxOptions = new FirefoxOptions();
-                firefoxOptions.addArguments("--headless");
-                driver = new FirefoxDriver(firefoxOptions);
-                break;
-            case "edge":
-                EdgeOptions edgeOptions = new EdgeOptions();
-                edgeOptions.addArguments("--headless");
-                driver = new EdgeDriver(edgeOptions);
-                break;
-            case "safari":
-                driver = new SafariDriver();
-                break;
-            default:
-                throw new RuntimeException("Unsupported browser: " + browser);
-        }
-        this.wait = new WebDriverWait(driver, Duration.ofSeconds(2));
+        this.driver = getDriver();
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
     @AfterEach
@@ -70,30 +82,31 @@ public class FilmUITest {
     @Test
     @DisplayName("Añadir una nueva película y comprobar que se ha creado")
     public void createFilmTest() throws Exception {
+        if (driver != null) {
+            // GIVEN: Partiendo de que estamos en la página principal de la web
+            this.driver.get("http://localhost:" + this.port + "/");
 
-        // GIVEN: Partiendo de que estamos en la página principal de la web
-        this.driver.get("http://localhost:" + this.port + "/");
+            // WHEN: Creamos un nueva película
 
-        // WHEN: Creamos un nueva película
+            String title = "Spider-Man: No Way Home";
+            String synopsis = "Peter Parker es desenmascarado y por tanto no es capaz de separar su vida normal de los enormes riesgos que conlleva ser un súper héroe.";
+            String url = "https://www.themoviedb.org/t/p/w220_and_h330_face/osYbtvqjMUhEXgkuFJOsRYVpq6N.jpg";
+            String year = "2021";
 
-        String title = "Spider-Man: No Way Home";
-        String synopsis = "Peter Parker es desenmascarado y por tanto no es capaz de separar su vida normal de los enormes riesgos que conlleva ser un súper héroe.";
-        String url = "https://www.themoviedb.org/t/p/w220_and_h330_face/osYbtvqjMUhEXgkuFJOsRYVpq6N.jpg";
-        String year = "2021";
+            // Hacemos click en "New film"
+            driver.findElement(By.xpath("//*[text()='New film']")).click();
+            // Rellenamos el formulario
+            driver.findElement(By.name("title")).sendKeys(title);
+            driver.findElement(By.name("url")).sendKeys(url);
+            driver.findElement(By.name("releaseYear")).sendKeys(year);
+            driver.findElement(By.name("synopsis")).sendKeys(synopsis);
+            // Enviamos el formulario
+            driver.findElement(By.id("Save")).click();
 
-        // Hacemos click en "New film"
-        driver.findElement(By.xpath("//*[text()='New film']")).click();
-        // Rellenamos el formulario
-        driver.findElement(By.name("title")).sendKeys(title);
-        driver.findElement(By.name("url")).sendKeys(url);
-        driver.findElement(By.name("releaseYear")).sendKeys(year);
-        driver.findElement(By.name("synopsis")).sendKeys(synopsis);
-        // Enviamos el formulario
-        driver.findElement(By.id("Save")).click();
+            // THEN: Esperamos que la película creada aparezca en la nueva página resultante
 
-        // THEN: Esperamos que la película creada aparezca en la nueva página resultante
-
-        this.wait.until(ExpectedConditions.textToBe(By.id("film-title"), title));
+            this.wait.until(ExpectedConditions.textToBe(By.id("film-title"), title));
+        }
     }
 
 }
